cd78b263b01f5435fce91cda8783c0806bf91efc
DEDC-git <com@declicetdesclaps.fr>
2025-09-22 15:34:18 +0200

    fix: Make ProjectLayout form responsive
    - Implemented React-Bootstrap's Row and Col components for responsive form layout in ProjectLayout.jsx.
    - Added missing Row and Col imports to ProjectLayout.jsx.

--- client/src/components/layouts/ProjectLayout.jsx
+++ client/src/components/layouts/ProjectLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button, Form } from "react-bootstrap";
+import { Container, Button, Form, Row, Col } from "react-bootstrap";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "../../App.css";
@@ -51,148 +51,225 @@ export default function ProjectLayout({
     <div className="mb-5 clearfix">
       {isEditable && isEditing ? (
         <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>Titre</Form.Label>
-            <Form.Control
-              type="text"
-              name="title"
-              value={formData.title || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Sous-titre</Form.Label>
-            <Form.Control
-              type="text"
-              name="subtitle"
-              value={formData.subtitle || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Article</Form.Label>
-            <Form.Control
-              as="textarea"
-              name="article"
-              value={formData.article || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Panel</Form.Label>
-            <Form.Control
-              type="text"
-              name="panel"
-              value={formData.panel || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Short Title</Form.Label>
-            <Form.Control
-              type="text"
-              name="shortitle"
-              value={formData.shortitle || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Short Text</Form.Label>
-            <Form.Control
-              as="textarea"
-              name="shortext"
-              value={formData.shortext || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Contacts</Form.Label>
-            <Form.Control
-              type="text"
-              name="contacts"
-              value={formData.contacts || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 1 URL</Form.Label>
-            <Form.Control
-              type="text"
-              name="links01"
-              value={formData.links01 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 1 Type</Form.Label>
-            <Form.Control
-              type="text"
-              name="typelink01"
-              value={formData.typelink01 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 1 Nom</Form.Label>
-            <Form.Control
-              type="text"
-              name="namelink01"
-              value={formData.namelink01 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 2 URL</Form.Label>
-            <Form.Control
-              type="text"
-              name="links02"
-              value={formData.links02 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 2 Type</Form.Label>
-            <Form.Control
-              type="text"
-              name="typelink02"
-              value={formData.typelink02 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Lien 2 Nom</Form.Label>
-            <Form.Control
-              type="text"
-              name="namelink02"
-              value={formData.namelink02 || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Photographie</Form.Label>
-            <Form.Control
-              type="file"
-              name="image"
-              onChange={handleFileChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              Texte alternatif (au cas où l'imege ne s'affiche pas)
-            </Form.Label>
-            <Form.Control
-              type="text"
-              name="alt"
-              value={formData.alt || ""}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Button variant="success" onClick={handleSaveClick} className="me-2">
-            Sauvegarder
-          </Button>
-          <Button variant="secondary" onClick={handleCancelClick}>
-            Annuler
-          </Button>
+          <h4>TITRES</h4>
+          <Row>
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Titre principal</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="title"
+                  value={formData.title || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Sous-titre</strong> (date)
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="subtitle"
+                  value={formData.subtitle || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={4}>
+              <Form.Group className="mb-5">
+                <Form.Label>
+                  <strong>Titre encadré</strong> (visible dans Tous les projets)
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="panel"
+                  value={formData.panel || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+
+            <Col md={6} lg={4}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Titre raccourci</strong> (visible dans Tous les
+                  projets)
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="shortitle"
+                  value={formData.shortitle || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={4}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Texte raccourci</strong> (visible dans Tous les
+                  projets)
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="shortext"
+                  value={formData.shortext || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+
+            <h4>CONTACTS ET LIENS</h4>
+
+            <Col md={6} lg={12}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Contacts</strong> Nom de l'organisme
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="contacts"
+                  value={formData.contacts || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Adresse URL du contact</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="links01"
+                  value={formData.links01 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            {/* <Col md={6} lg={4}>
+              <Form.Group className="mb-3">
+                <Form.Label>Lien 1 Type</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="typelink01"
+                  value={formData.typelink01 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col> */}
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  {" "}
+                  <strong>Nom du premier lien</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="namelink01"
+                  value={formData.namelink01 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <strong>Adresse URL du 2ème lien</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="links02"
+                  value={formData.links02 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            {/* <Col md={6} lg={4}>
+              <Form.Group className="mb-3">
+                <Form.Label>Lien 2 Type</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="typelink02"
+                  value={formData.typelink02 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col> */}
+            <Col md={6} lg={6}>
+              <Form.Group className="mb-5">
+                <Form.Label>
+                  <strong>Nom du 2ème lien</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="namelink02"
+                  value={formData.namelink02 || ""}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            </Col>
+            <h4>VISUEL ET ARTICLE</h4>
+            <Row>
+              <Col md={12} lg={4}>
+                <Col md={6} lg={12}>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <strong>Photographie</strong>
+                    </Form.Label>
+                    <Form.Control
+                      type="file"
+                      name="image"
+                      onChange={handleFileChange}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6} lg={12}>
+                  <Form.Group className="mb-5">
+                    <Form.Label>
+                      <strong>Texte alternatif</strong> au cas où l'imege ne
+                      s'affiche pas
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="alt"
+                      value={formData.alt || ""}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                </Col>
+              </Col>
+
+              <Col xs={12} md={8}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Article</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    name="article"
+                    value={formData.article || ""}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Col xs={12} className="d-flex justify-content-end">
+              <Button
+                variant="success"
+                onClick={handleSaveClick}
+                className="me-2"
+              >
+                Sauvegarder
+              </Button>
+              <Button variant="secondary" onClick={handleCancelClick}>
+                Annuler
+              </Button>
+            </Col>
+          </Row>
         </Form>
       ) : (
         <>
