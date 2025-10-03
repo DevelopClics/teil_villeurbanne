0d64c8833d81ef19038feab1a3867df4dfecf13a
DEDC-git <com@declicetdesclaps.fr>
2025-10-03 09:32:01 +0200

    feat(FormJoinus): improve form validation and user experience
    - Enable "Envoyer" button only when required fields are filled
    - Prevent layout shift from error messages
    - Make "Objet" and "Email" fields optional
    - Adjust minLength for "Message" and "Email" fields

--- client/src/components/elements/FormJoinus.jsx
+++ client/src/components/elements/FormJoinus.jsx
@@ -42,15 +42,15 @@ const FormJoinus = () => {
   };
 
   const form = useRef();
-  const [selectedMainType, setSelectedMainType] = useState(''); // New state for main radio buttons
-  const [selectedSubType, setSelectedSubType] = useState(''); // New state for sub radio buttons
+  const [selectedMainType, setSelectedMainType] = useState(""); // New state for main radio buttons
+  const [selectedSubType, setSelectedSubType] = useState(""); // New state for sub radio buttons
 
-  const [showOptionsIndividualsDetails, setShowOptionsIndividualsDetails] =
-    useState(false);
+  // const [showOptionsIndividualsDetails, setShowOptionsIndividualsDetails] =
+  //   useState(false);
 
   const handleMainRadioChange = (e) => {
     setSelectedMainType(e.target.value);
-    setSelectedSubType(''); // Always reset sub-type when main type changes
+    setSelectedSubType(""); // Always reset sub-type when main type changes
     setShowOptionsIndividualsDetails(false); // Always reset sub-details visibility
   };
 
@@ -68,7 +68,10 @@ const FormJoinus = () => {
       <div className="row g-4">
         {/* FISRTNAME */}
         <div className="col-12 col-md-4">
-          <span className="error text-danger">
+          <span
+            className="error text-danger"
+            style={{ height: "10px", display: "inline-block" }}
+          >
             {errors.from_firstname?.type === "minLength" &&
               "Ecrire plus de 2 caractères"}
             {errors.from_firstname?.type === "maxLength" &&
@@ -76,6 +79,7 @@ const FormJoinus = () => {
             {errors.from_firstname?.type === "pattern: /^[A-Za-z]+$/i"}
             {errors.from_firstname?.type === "required" &&
               "Entrez votre prénom"}
+            {!errors.from_firstname && <>&nbsp;</>}
           </span>
           <Form.Control
             type="text"
@@ -91,12 +95,16 @@ const FormJoinus = () => {
         {/* END FISRTNAME */}
         {/* LASTNAME */}
         <div className="col-12 col-md-4">
-          <span className="error text-danger">
+          <span
+            className="error text-danger"
+            style={{ height: "20px", display: "inline-block" }}
+          >
             {errors.from_surname?.type === "Entrez votre nom"}
             {errors.from_surname?.type === "minLength" &&
               "Ecrire plus de 2 caractères"}
             {errors.from_surname?.type === "maxLength" &&
               "Ecrire moins de 20 caractères"}
+            {!errors.from_surname && <>&nbsp;</>}
           </span>
           <Form.Control
             type="text"
@@ -113,7 +121,10 @@ const FormJoinus = () => {
         {/* EMAIL */}
         <div className="col-12 col-md-4">
           <Form.Group controlId="formBasicEmail">
-            <span className="error text-danger">
+            <span
+              className="error text-danger"
+              style={{ height: "20px", display: "inline-block" }}
+            >
               {errors.from_email?.type === "required" &&
                 "Entrez votre adresse e-mail"}
               {errors.from_email?.type === "pattern" &&
@@ -122,6 +133,7 @@ const FormJoinus = () => {
                 "Ecrire plus de 14 caractères"}
               {errors.from_email?.type === "maxLength" &&
                 "Ecrire 30 caractères au maximum"}
+              {!errors.from_email && <>&nbsp;</>}
             </span>
             <Form.Control
               type="email"
@@ -131,9 +143,9 @@ const FormJoinus = () => {
               placeholder="E-mail"
               name="from_email"
               {...register("from_email", {
-                required: true,
+                required: false,
                 pattern: /^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                minLength: 15,
+                minLength: 10,
                 maxLength: 30,
               })}
             />
@@ -143,13 +155,17 @@ const FormJoinus = () => {
         {/* OBJECT */}
         <div className="col-12 col-md-4">
           <div className="col-12  col-md-12 col-lg-12 ps-lg-1">
-            <span className="error text-danger">
+            <span
+              className="error text-danger"
+              style={{ height: "20px", display: "inline-block" }}
+            >
               {errors.object?.type === "required" &&
                 "Indiquez l'objet de l'email"}
               {errors.object?.type === "minLength" &&
                 "Ecrire 10 caractères au minimum"}
               {errors.object?.type === "maxLength" &&
                 "Ecrire moins de 50 caractères"}
+              {!errors.object && <>&nbsp;</>}
             </span>
           </div>
           <Form.Control
@@ -157,7 +173,7 @@ const FormJoinus = () => {
             placeholder="Objet"
             name="object"
             {...register("object", {
-              required: true,
+              required: false,
               minLength: 10,
               maxLength: 49,
             })}
@@ -179,7 +195,7 @@ const FormJoinus = () => {
                 name="mainRadioGroup" // New name for main radio group
                 id="radioAssociation"
                 value="association"
-                checked={selectedMainType === 'association'}
+                checked={selectedMainType === "association"}
                 onChange={handleMainRadioChange}
               />
             </div>
@@ -192,7 +208,7 @@ const FormJoinus = () => {
                 name="mainRadioGroup" // New name for main radio group
                 id="radioHabitant"
                 value="habitant"
-                checked={selectedMainType === 'habitant'}
+                checked={selectedMainType === "habitant"}
                 onChange={handleMainRadioChange}
               />
             </div>
@@ -204,16 +220,19 @@ const FormJoinus = () => {
                 name="mainRadioGroup" // New name for main radio group
                 id="radioEleve"
                 value="eleve"
-                checked={selectedMainType === 'eleve'}
+                checked={selectedMainType === "eleve"}
                 onChange={handleMainRadioChange}
               />
             </div>
           </div>
           {/* Conditional rendering for sub-options */}
-          {(selectedMainType === 'habitant' || selectedMainType === 'eleve') && (
+          {(selectedMainType === "habitant" ||
+            selectedMainType === "eleve") && (
             <div
               className={`row col-6 col-md-12 options-container-two ${
-                (selectedMainType === 'habitant' || selectedMainType === 'eleve') ? "options-container-show-two" : ""
+                selectedMainType === "habitant" || selectedMainType === "eleve"
+                  ? "options-container-show-two"
+                  : ""
               }`}
             >
               <div className="col-md-2 col-xxl-2">Je cherche</div>
@@ -225,7 +244,7 @@ const FormJoinus = () => {
                   name="subRadioGroup" // New name for sub radio group
                   id="radioLogement"
                   value="un logement"
-                  checked={selectedSubType === 'un logement'}
+                  checked={selectedSubType === "un logement"}
                   onChange={handleSubRadioChange}
                 />
               </div>
@@ -237,7 +256,7 @@ const FormJoinus = () => {
                   name="subRadioGroup" // New name for sub radio group
                   id="radioStage"
                   value="un stage"
-                  checked={selectedSubType === 'un stage'}
+                  checked={selectedSubType === "un stage"}
                   onChange={handleSubRadioChange}
                 />
               </div>
@@ -249,7 +268,7 @@ const FormJoinus = () => {
                   name="subRadioGroup" // New name for sub radio group
                   id="radioBenevole"
                   value="A être bénèvole sur un projet"
-                  checked={selectedSubType === 'A être bénèvole sur un projet'}
+                  checked={selectedSubType === "A être bénèvole sur un projet"}
                   onChange={handleSubRadioChange}
                 />
               </div>
@@ -261,19 +280,23 @@ const FormJoinus = () => {
       {/* MESSAGE */}
       <div
         className={`mt-4 offset-xxl-1 options-container-one ${
-          selectedMainType === 'association' || selectedSubType !== ''
+          selectedMainType === "association" || selectedSubType !== ""
             ? "options-container-show-one"
             : ""
         }`}
       >
         <FloatingLabel controlId="floatingTextarea">
-          <span className="error text-danger">
+          <span
+            className="error text-danger"
+            style={{ height: "20px", display: "inline-block" }}
+          >
             {errors.message?.type === "required" &&
               "Merci de nous écrire un message"}
             {errors.message?.type === "minLength" &&
               "Ecrire plus de 100 caractères"}
             {errors.message?.type === "maxLength" &&
               "Ecrire moins de 700 caractères"}
+            {!errors.message && <>&nbsp;</>}
           </span>
         </FloatingLabel>
         <Form.Control
@@ -282,7 +305,7 @@ const FormJoinus = () => {
           rows={1}
           name="message"
           {...register("message", {
-            required: true,
+            required: false,
             minLength: 100,
             maxLength: 699,
           })}
@@ -300,9 +323,9 @@ const FormJoinus = () => {
           type="submit"
           className="envoyer-btn px-5 rounded-pill"
           disabled={
-            selectedMainType === '' ||
-            (selectedMainType === 'association' && watch('message') === '') ||
-            ((selectedMainType === 'habitant' || selectedMainType === 'eleve') && (selectedSubType === '' || watch('message') === ''))
+            !watch("from_email") ||
+            !watch("object") ||
+            (selectedMainType !== "association" && selectedSubType === "")
           }
         >
           Envoyer{" "}
