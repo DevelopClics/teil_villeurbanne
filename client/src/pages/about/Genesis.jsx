dccf69ce8ae47ed60ecda80f2151ab1b16f169f4
DEDC-git <com@declicetdesclaps.fr>
2025-10-15 15:12:55 +0200

    feat(genesis): show admin buttons on carousel for authenticated users

--- client/src/pages/about/Genesis.jsx
+++ client/src/pages/about/Genesis.jsx
@@ -3,9 +3,11 @@ import Breadcrumbs from "../../components/breadcrumbs/Breadcrumbs";
 import CarouselComponent from "../../components/Carousel/Carousel";
 import PageLayout from "../../components/layouts/PageLayout";
 import GenesisComp from "../../components/GenesisComp";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Genesis({ isNavbarHovered }) {
   const SUB = "La genèse";
+  const { isAuthenticated } = useAuth();
 
   return (
     <>
@@ -14,6 +16,7 @@ export default function Genesis({ isNavbarHovered }) {
         category="genesis"
         carouselTextId={1}
         startFaded={true}
+        isEditable={isAuthenticated}
       />
       <Breadcrumbs breadcrumbsnav="Qui sommes-nous ?" breadcrumbssub={SUB} />
       <PageLayout
